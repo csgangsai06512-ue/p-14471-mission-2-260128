363cfb00edde15da6ba235489de50b382664c54f
csgangsai06512-ue <csgangsai06512@gmail.com>
2026-02-23 16:52:10 +0900

    Update Main.java

--- 명언 java 코드/Main.java
+++ 명언 java 코드/Main.java
@@ -15,10 +15,10 @@ public class Main {
 
         System.out.println("======================");
 		System.out.println("과제 1 : JAVA 명언 게시판");
-		System.out.println("======================");	
+		System.out.println("======================");
 
         while (true) {
-            System.out.print("(1)등록, (2)출력 (0)종료 명령은 : ");
+            System.out.print("(1)등록, (2)출력 (3)삭제 (4)수정 (0)종료 명령은 : ");
             int input_cmd = Integer.parseInt(sc.nextLine());
 
             if (input_cmd==0) {
@@ -35,14 +35,67 @@ public class Main {
             } else if (input_cmd==2) {
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("-------------------");
-                
+               
                 if (WiseSays.isEmpty()) {
                     System.out.println("등록된 명언이 없습니다.");
                 } else {
-                    WiseSay latest = WiseSays.get(WiseSays.size() - 1);
-                    System.out.println(latest.toString());
+					for (int i = 0; i < WiseSays.size(); i++) {    
+						System.out.println(WiseSays.get(i).toString());
+					}
                 }
+            } else if(input_cmd==3) {
+                System.out.print("삭제 번호 : ");
+                String str_del = sc.nextLine();
+                int n_del = Integer.parseInt(str_del);
+                boolean bDel = false;
+
+                if (WiseSays.isEmpty()) {
+                    System.out.println("등록된 명언이 없습니다.");
+                } else {
+
+					for (int i = 0; i < WiseSays.size(); i++) {
+						if (WiseSays.get(i).getId() == n_del) {      
+							System.out.println("삭제 번호 : " + WiseSays.get(i).getId());
+							bDel = true;
+							WiseSays.remove(WiseSays.get(i));  
+							break;
+						}  
+					}
+				}
+				
+				if(!bDel) { 
+					System.out.println(str_del + "번 등록된 명언이 없습니다.");  
+				}			
+				
+            } else if (input_cmd==4) {
+                System.out.print("수정 번호 : ");
+                String modify = sc.nextLine();
+                int n_modify = Integer.parseInt(modify);
+				boolean bModify = false;
+
+
+                if (WiseSays.isEmpty()) {
+                    System.out.println("등록된 명언이 없습니다.");
+                } else {
+					for (int i = 0; i < WiseSays.size(); i++) {
+						if (WiseSays.get(i).getId() == n_modify) {
+							System.out.print("명언 입력 : ");
+							String content = sc.nextLine(); 
+							WiseSays.get(i).setContent(content);  
+							System.out.print("작가 입력 : ");
+							String author = sc.nextLine(); 
+							WiseSays.get(i).setAuthor(author);
+							bModify = true;
+						}
+					}                                      
+				}
+				
+				if(!bModify) { 
+					System.out.println(modify + "번 등록된 명언이 없습니다.");  
+				}
+				
             }
+
         }
         sc.close();
     }
